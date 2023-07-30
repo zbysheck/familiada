dfb4c218900dcc3e47f45eebf9d7881fe748233c
zbyszek <zbyshekh@gmail.com>
2023-07-31 01:59:54 +0200

    screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,54 @@
 <!DOCTYPE html>
 <html>
 <body>
-<button onclick="sendMessage()">Send Message</button>
-<p id="messages"></p>
+<input id="value1" value="kopiowanie"><button id="myButton1">odp 1</button><br/>
+<input id="value2" value="usuwanie"><button id="myButton2">odp 2</button><br/>
+<input id="value3" value="wyświetlanie"><button id="myButton3">odp 3</button><br/>
+<input id="value4" value="przenoszenie"><button id="myButton4">odp 4</button><br/>
+<input id="value5" value="tworzenie"><button id="myButton5">odp 5</button><br/>
+<input id="value6" value="patryk ssie pałke"><button id="myButton6">odp 6</button><br/>
 
 <script>
-    const connection = new WebSocket('ws://localhost:8080');
+    var conn = new WebSocket('ws://localhost:8080');
 
-    connection.onopen = () => {
-        console.log('Connection open');
+    conn.onopen = function(e) {
+        console.log("Połączenie z serwerem WebSocket zostało nawiązane!");
     };
 
-    connection.onerror = (error) => {
-        console.log(`WebSocket error: ${error}`);
-    };
-
-    connection.onmessage = (e) => {
+    conn.onmessage = function(e) {
         console.log(e.data);
-        document.getElementById('messages').innerText += '\n' + e.data;
     };
 
-    function sendMessage() {
-        connection.send('Hello from client!');
-    }
+    document.getElementById('myButton1').addEventListener('click', function() {
+        let value = document.getElementById('value1').value
+        conn.send(`1,${value},34`);
+    });
+
+    document.getElementById('myButton2').addEventListener('click',function() {
+        let value = document.getElementById('value2').value
+        conn.send(`2,${value},28`);
+    });
+
+    document.getElementById('myButton3').addEventListener('click',function() {
+        let value = document.getElementById('value3').value
+        conn.send(`3,${value},18`);
+    });
+
+    document.getElementById('myButton4').addEventListener('click',function() {
+        let value = document.getElementById('value4').value
+        conn.send(`4,${value},11`);
+    });
+
+    document.getElementById('myButton5').addEventListener('click',function() {
+        let value = document.getElementById('value5').value
+        conn.send(`5,${value},9`);
+    });
+
+    document.getElementById('myButton6').addEventListener('click',function() {
+        let value = document.getElementById('value6').value
+        conn.send(`6,${value},1`);
+    });
 </script>
 </body>
 </html>
+
---
+++ websocket.php
@@ -0,0 +1,48 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class MyChat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        $conn->close();
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            if ($from != $client) {
+                $client->send($msg);
+            }
+        }
+    }
+}
+
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new MyChat()
+        )
+    ),
+    8080
+);
+
+$server->run();
